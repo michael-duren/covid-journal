ce89ad9028f9db78bdbc4c4a97c1bf856d03d331
Michael Duren <141460708+460577@users.noreply.github.com>
2024-07-28 18:19:23 -0500

    add home & nav

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,11 +1,11 @@
 package server
 
 import (
+	"covid-journal/cmd/web"
 	"encoding/json"
 	"log"
 	"net/http"
 
-	"covid-journal/cmd/web"
 	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -15,14 +15,18 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", s.HelloWorldHandler)
+	// r.Get("/", s.HelloWorldHandler)
 
 	r.Get("/health", s.healthHandler)
 
 	fileServer := http.FileServer(http.FS(web.Files))
 	r.Handle("/assets/*", fileServer)
-	r.Get("/web", templ.Handler(web.HelloForm()).ServeHTTP)
-	r.Post("/hello", web.HelloWebHandler)
+	// r.Get("/web", templ.Handler(web.HelloForm()).ServeHTTP)
+	// r.Post("/hello", web.HelloWebHandler)
+
+	r.Get("/", templ.Handler(web.HomePage()).ServeHTTP)
+	r.Get("/journal", templ.Handler(web.JournalPage()).ServeHTTP)
+	r.Get("/about", templ.Handler(web.AboutPage()).ServeHTTP)
 
 	return r
 }
